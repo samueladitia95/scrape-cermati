a95faea16099867272ff6e546cd62b631559c301
Samuel Aditia <samueladitia95@gmail.com>
2020-11-27 22:10:37 +0700

    add comment, fix minor bug

--- solution.js
+++ solution.js
@@ -6,6 +6,7 @@ const cermatiURL = "https://www.cermati.com";
 
 const main = async () => {
   try {
+    // get all the artcle link
     let { data } = await axios.get(`${cermatiURL}/artikel`);
     let $ = await cheerio.load(data);
     let linkToArticles = $(".article-list-item").map((i, el) => {
@@ -16,6 +17,7 @@ const main = async () => {
       articles: [],
     };
 
+    // go to each article
     linkToArticles.each(async (i, el) => {
       try {
         let { data } = await axios.get(`${cermatiURL}${el}`);
@@ -27,7 +29,11 @@ const main = async () => {
           postingDate: $(".post-date").text().trim(),
           relatedArticles: [],
         };
+
+        // scrape realted article link and title
         $(".item-title").each((i, el) => {
+          // no unique id or class for related article
+          // related article is the first 5 element
           if (i < 5) {
             postData.relatedArticles.push({
               url: `${cermatiURL}${$(el).parent().attr("href")}`,
@@ -35,6 +41,9 @@ const main = async () => {
             });
           }
         });
+
+        // works, but inefficient
+        // async problem, have to write like this, need fix
         articles.articles.push(postData);
         fs.writeFileSync("./solution.json", JSON.stringify(articles, null, 2));
       } catch (err) {
